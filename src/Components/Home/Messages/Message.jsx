a1a4b7fb3adf4447697662aa66636757e48ac1c6
Dr.rabi3 <abdalrazekmohmed6@gmail.com>
2024-06-10 19:27:07 +0300

    rabi3 3l code

--- src/Components/Home/Messages/Message.jsx
+++ src/Components/Home/Messages/Message.jsx
@@ -1,84 +1,41 @@
 import React from 'react';
 import './Message.css'; // Assuming you save the CSS styles in Chat.css
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const Message = () => {
   return (
-    <div className="container py-5 px-4">
-      {/* For demo purpose */}
-      <header className="text-center">
-        <h1 className="display-4 text-white">Bootstrap Chat</h1>
-        <p className="text-white lead mb-0">An elegant chat widget compatible with Bootstrap 4</p>
-        <p className="text-white lead mb-4">Snippet by
-          <a href="https://bootstrapious.com" className="text-white">
-            <u>Bootstrapious</u></a>
-        </p>
-      </header>
-
+    <div className="container py-5 px-4 h-100">
       <div className="row rounded-lg overflow-hidden shadow">
         {/* Users box */}
-        <div className="col-5 px-0">
+        <div className="col-4 px-0">
           <div className="bg-white">
             <div className="bg-gray px-4 py-2 bg-light">
               <p className="h5 mb-0 py-1">Recent</p>
             </div>
             <div className="messages-box">
               <div className="list-group rounded-0">
-                <User
-                  active
-                  name="Jason Doe"
-                  date="25 Dec"
-                  message="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore."
-                />
-                <User
-                  name="Jason Doe"
-                  date="14 Dec"
-                  message="Lorem ipsum dolor sit amet, consectetur. incididunt ut labore."
-                />
-                <User
-                  name="Jason Doe"
-                  date="9 Nov"
-                  message="consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore."
-                />
-                <User
-                  name="Jason Doe"
-                  date="18 Oct"
-                  message="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore."
-                />
-                <User
-                  name="Jason Doe"
-                  date="17 Oct"
-                  message="consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore."
-                />
-                <User
-                  name="Jason Doe"
-                  date="2 Sep"
-                  message="Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                />
-                <User
-                  name="Jason Doe"
-                  date="30 Aug"
-                  message="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore."
-                />
-                <User
-                  name="Jason Doe"
-                  date="21 Aug"
-                  message="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore."
-                />
+                <User active name="Jason Doe" date="25 Dec" />
+                <User name="Jason Doe" date="14 Dec" />
+                <User name="Jason Doe" date="9 Nov" />
+                <User name="Jason Doe" date="18 Oct" />
+                <User name="Jason Doe" date="17 Oct" />
+                <User name="Jason Doe" date="2 Sep" />
+                <User name="Jason Doe" date="30 Aug" />
+                <User name="Jason Doe" date="21 Aug" />
               </div>
             </div>
           </div>
         </div>
 
         {/* Chat Box */}
-        <div className="col-7 px-0">
+        <div className="col-8 px-0">
           <div className="px-4 py-5 chat-box bg-white">
-            {/* Sender Message */}
             <ChatMessage
               sender
               time="12:00 PM | Aug 13"
               message="Test which is a new approach all solutions"
             />
-            {/* Receiver Message */}
             <ChatMessage
               time="12:00 PM | Aug 13"
               message="Test which is a new approach to have all solutions"
@@ -120,8 +77,13 @@ const Message = () => {
                 className="form-control rounded-0 border-0 py-4 bg-light"
               />
               <div className="input-group-append">
-                <button id="button-addon2" type="submit" className="btn btn-link">
-                  <i className="fa fa-paper-plane"></i>
+                <button
+                  id="button-addon2"
+                  type="submit"
+                  className="btn btn-link"
+                >
+                  {/* <i className="fa fa-paper-plane"></i> */}
+                  <FontAwesomeIcon icon={faPaperPlane} />
                 </button>
               </div>
             </div>
@@ -155,7 +117,11 @@ const User = ({ active, name, date, message }) => (
 );
 
 const ChatMessage = ({ sender, time, message, primary }) => (
-  <div className={`media w-50 mb-3 ${sender ? '' : 'ml-auto'}`}>
+  <div
+    className={`media w-50 mb-3 ${sender ? "" : "ml-auto"} ${
+      primary ? "ms-auto" : ""
+    }`}
+  >
     {!sender && (
       <img
         src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg"
@@ -165,8 +131,16 @@ const ChatMessage = ({ sender, time, message, primary }) => (
       />
     )}
     <div className="media-body ml-3">
-      <div className={`rounded py-2 px-3 mb-2 ${primary ? 'bg-primary' : 'bg-light'}`}>
-        <p className={`text-small mb-0 ${primary ? 'text-white' : 'text-muted'}`}>{message}</p>
+      <div
+        className={`rounded py-2 px-3 mb-2 ${
+          primary ? "bg-primary" : "bg-light"
+        }`}
+      >
+        <p
+          className={`text-small mb-0 ${primary ? "text-white" : "text-muted"}`}
+        >
+          {message}
+        </p>
       </div>
       <p className="small text-muted">{time}</p>
     </div>
